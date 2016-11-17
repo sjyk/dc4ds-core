13b46d01405c95a4da93decaf8ea112936d6196f
Sanjay Krishnan <sanjaykrishn@gmail.com>
2016-11-16 21:40:58 -0800

    Added code to support learning

--- dc4ds/dataset.py
+++ dc4ds/dataset.py
@@ -4,8 +4,10 @@ A dataset is a wrapper class for a Pandas Data Frame that also
 contains logical assertions about data quality.
 """
 import pandas
+import numpy as np
 import warnings
 from dc4ds.loaders.TypeInference import TypeInference
+from dc4ds.utils.feature_utils import *
 
 class Dataset(object):
 
@@ -21,8 +23,12 @@ class Dataset(object):
         """
 
         #loads the data with the provided args
-        self.loadedData = loader.load()
-        self.types = TypeInference().getDataTypes(self.loadedData)
+        loadedData = loader.load()
+
+        #make it immutable
+        self.loadedData = tuple(tuple(row) for row in loadedData)
+
+        self.types = TypeInference().getDataTypes(loadedData)
 
         pandasData = {}
         for i in range(len(self.types)):
@@ -79,3 +85,77 @@ class Dataset(object):
                     errors.add((row, col))
 
         return errors
+
+
+
+
+
+class FeaturizedDataset(Dataset):
+    """
+    A featurized dataset extends a dataset to also include features
+    """
+
+    def __init__(self,
+                loader, 
+                index=None, 
+                columns=None, 
+                dtype=None, 
+                copy=False):
+
+        super(FeaturizedDataset, self).__init__(loader, index, columns, dtype, copy)
+
+
+    def getFeatures(self, label_column=None):
+        """
+        Turns a data frame into a numpy matrix and label column if avail
+        """
+        if label_column == None:
+
+            features, transforms, fmap = featurize(self.loadedData, self.types)
+
+            return FeatureStruct(features, transforms, fmap, None, None, None)
+
+        else:
+
+            index = np.where(self.df.columns.values == label_column)[0]
+            features = [[col for i, col in enumerate(row) if i != index] for row in self.loadedData]
+            feature_types = [col for i, col in enumerate(self.types) if i != index]
+
+            nfeatures, transforms, fmap = featurize(features, feature_types)
+
+            labels = [row[index] for row in self.loadedData]
+            label_type = self.types[index]
+            nlabels, label_transform = labelize(labels, label_type)
+            
+            return FeatureStruct(nfeatures, 
+                                 transforms, 
+                                 fmap, 
+                                 label_column, 
+                                 nlabels, 
+                                 label_transform)
+
+
+
+
+class FeatureStruct(object):
+    """
+    This dataset defines a data structure that holds
+    all of the important feature variables
+    """
+
+    def __init__(self,
+                 features,
+                 transforms,
+                 fmap,
+                 label_column,
+                 labels,
+                 label_transform):
+
+        self.features = features
+        self.labels = labels
+        self.transforms = transforms
+        self.fmap = fmap
+        self.label_column = label_column
+
+
+
